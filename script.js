c81f166ac5b3dfdb88b9cc7685478158a9cac6cd
Alecu <alecugajos1@gmail.com>
2024-05-31 08:21:49 +0200

    commit

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const openBtn = document.querySelector('.openBtn');
 let mobNav = document.querySelector('.mobNav');
 
 closeBtn.addEventListener('click', () => {
-    mobNav.classList.remove('openeMobNav');
+    mobNav.classList.remove('openMobNav');
     closeBtn.classList.add('hidden');
     openBtn.classList.remove('hidden');
 });
 
 openBtn.addEventListener('click', () => {
-    mobNav.classList.add('openeMobNav');
+    mobNav.classList.add('openMobNav');
     openBtn.classList.add('hidden');
     closeBtn.classList.remove('hidden');
 });
 
 mobNav.addEventListener('click', () => {
-    mobNav.classList.remove('openeMobNav');
+    mobNav.classList.remove('openMobNav');
     closeBtn.classList.add('hidden');
     openBtn.classList.remove('hidden');
 });
